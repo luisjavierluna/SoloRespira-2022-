5b29dd6646a2e956f33f3a8d659c96e8d70a0850
luisjavierluna <javierlunaluis@gmail.com>
2023-03-19 19:00:27 -0600

    PART

--- SoloRespira-UI/src/app/app.module.ts
+++ SoloRespira-UI/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderLogoComponent } from './shared/header/header-logo/header-logo.component';
+import { HeaderMenuComponent } from './shared/header/header-menu/header-menu.component';
+import { HeaderIconsComponent } from './shared/header/header-icons/header-icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderLogoComponent,
+    HeaderMenuComponent,
+    HeaderIconsComponent
   ],
   imports: [
     BrowserModule,

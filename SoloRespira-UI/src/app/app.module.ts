81717fe56e1537bc69103bbc2f2d676de8e8e49f
luisjavierluna <javierlunaluis@gmail.com>
2023-04-02 15:17:50 -0600

    Add Header (responsive navbar)

--- SoloRespira-UI/src/app/app.module.ts
+++ SoloRespira-UI/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderLogoComponent } from './shared/header/header-logo/header-logo.component';
+import { HeaderMenuComponent } from './shared/header/header-menu/header-menu.component';
+import { HeaderIconsComponent } from './shared/header/header-icons/header-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeaderLogoComponent,
+    HeaderMenuComponent,
+    HeaderIconsComponent
   ],
   imports: [
     BrowserModule,
---
+++ SoloRespira-UI/src/app/shared/header/header-icons/header-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-icons',
+  templateUrl: './header-icons.component.html',
+  styleUrls: ['./header-icons.component.scss']
+})
+export class HeaderIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
